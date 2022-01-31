ffcca777c9c80d56fabe5ea03ed99bc902b8fff5
ellis <ellis@rwest.io>
2022-01-31 17:08:46 -0500

    cargo organizing

--- src/elisp/mpk-org.el
+++ src/elisp/mpk-org.el
@@ -1,30 +1,29 @@
 ;;; mpk-org.el --- mpk org extensions -*- lexical-binding: t; -*-
-;; 
+
 ;; Copyright (C) 2021  ellis
-;; 
+
 ;; Author: ellis <ellis@rwest.io>
 ;; Keywords: multimedia, extensions
-;; 
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;; 
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; 
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-;; 
+
 ;;; Commentary:
 ;; 
 ;; 
-;; 
+
 ;;; Code:
-
-
+(require 'mpk)
 
 (provide 'mpk-org)
 ;;; mpk-org.el ends here
